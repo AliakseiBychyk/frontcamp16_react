f19a92ec5d0c1403685d4397d4d2925b8dedad9c
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-16 19:05:59 +0300

    favicon applied

---
+++ public/app.js
@@ -0,0 +1,10 @@
+"use strict"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const hello = (
+  <h1>Aleks's blog home page</h1>
+);
+
+ReactDOM.render(hello, document.getElementById('root'));
+
