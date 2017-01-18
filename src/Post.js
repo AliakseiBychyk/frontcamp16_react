a3164b3faba01e60d7dd2bf8095c371bbb2757a9
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-18 16:09:37 +0300

    indents formated

--- src/Post.js
+++ src/Post.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
 
 export default class Post extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            post: {}
-        };
+    this.state = {
+      post: {}
+    };
 
-        this.fetchPost = this.fetchPost.bind(this);
-    }
+    this.fetchPost = this.fetchPost.bind(this);
+  }
 
-    componentWillMount() {
-        this.fetchPost(this.props.params.postId);
-    }
+  componentWillMount() {
+    this.fetchPost(this.props.params.postId);
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    this.fetchPost(nextProps.params.postId);
+  }
 
-    componentWillReceiveProps(nextProps) {
-        this.fetchPost(nextProps.params.postId);
-    }
+  fetchPost(postId) {
+    console.log(this.props.params.postId)
+    fetch(`http://localhost:8000/blog/${postId}`)
+      .then(response => response.json())
+      .then((post) => {
+        console.log(post)
+        this.setState({post: post})
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    fetchPost(postId) {
-        console.log(this.props.params.postId)
-        fetch(`http://localhost:8000/blog/${postId}`)
-          .then(response => response.json())
-          .then((post) => {
-            console.log(post)
-            this.setState({post: post})
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-    }
-
-    render() {
-      return (
-        <div>
-          {this.props.params.postId}
-          <h3>{this.state.post.author}</h3>
-          <h1>{this.state.post.title}</h1>
-          <h2>{this.state.post.body}</h2>
-          <h4>Tags:</h4> 
-          <h4>{this.state.post.tags.map(function(tag) {
-            return <span>{tag} </span>;
-          })}
-          </h4>               
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        {this.props.params.postId}
+        <h3>{this.state.post.author}</h3>
+        <h1>{this.state.post.title}</h1>
+        <h2>{this.state.post.body}</h2>
+        <h4>Tags:</h4> 
+        <h4>{this.state.post.tags.map(function(tag) {
+          return <span>{tag} </span>;
+        })}
+        </h4>               
+      </div>
+    );
+  }
 }
--- src/Posts.js
+++ src/Posts.js
@@ -3,41 +3,41 @@ import Post from './Post';
 import { Link } from 'react-router';
 
 export default class Posts extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            posts: []
-        };
-    }
+    this.state = {
+      posts: []
+    };
+  }
 
-    componentDidMount() {
-      fetch('http://localhost:8000/blog')
-            .then(response => response.json())
-            .then((data) => {
-                this.setState({posts: data.posts})
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
+  componentDidMount() {
+    fetch('http://localhost:8000/blog')
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({posts: data.posts})
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    render() {
-        return (
-          <div>
-              <h1> Here are some posts </h1>
-              <div>
-                <ul>
-                {this.state.posts.map(function (post, id) {
-                  return (
-                    <li>
-                      <Link to={`/${id}`} key={id}>{post.title}</Link>
-                    </li>);
-                })}
-                </ul>
-                {this.props.children}
-              </div>
+  render() {
+    return (
+      <div>
+        <h1> Here are some posts </h1>
+        <div>
+          <ul>
+            {this.state.posts.map(function (post, id) {
+              return (
+                <li>
+                  <Link to={`/${id}`} key={id}>{post.title}</Link>
+                </li>);
+            })}
+          </ul>
+            {this.props.children}
           </div>
-        );
-    }
+      </div>
+    );
+  }
 }
