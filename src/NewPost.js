c6647dc0ccba03b096ea3da12f4fe6fae0e64689
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-21 14:37:42 +0300

    NewPost input form

--- src/NewPost.js
+++ src/NewPost.js
@@ -19,9 +19,20 @@ export default class NewPost extends React.Component {
             <input className="form-control" type="text" name="body" placeholder="Post body" />
           </div>
           <div className="form-group">
+            <input className="form-control" type="text" name="permalink" placeholder="Permalink" />
+          </div>
+          <div className="form-group">
             <input className="form-control" type="text" name="author" placeholder="Author" />
           </div>
+          <div className="form-group">
+            <input className="form-control" type="text" name="tags" placeholder="Tags" />
+          </div>
+          <div className="form-group">
+            <input className="form-control" type="text" name="date" placeholder="Date" />
+          </div>
         </form>
+        <button className="btn btn-default" type="submit">Submit</button>
+
         <h4><Link to="/blog">Back to blog</Link></h4>
       </div>
     );
