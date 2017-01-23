ea5ac4b33cc69ddcb65d4cacc223d79942284876
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-01-24 00:48:29 +0300

    home page changes

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 export default class Home extends Component {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
+
+    const greetings = "WELCOME TO ALEKS\'S BLOG";
 
   render() {
     return (
-      <div className="container">
-        <h1>WELCOME TO ALEKS'S BLOG</h1>
-        <h4><Link to="/login">Log in</Link></h4>
-        <h4><Link to="/register">Sing up</Link></h4>
+      <div className = "container" >
+        <h1> {greetings} </h1>
+        {user && < h3 > You are currntly logged in as {user.username} < /h3>}
+        <h4> <Link to = "/login" > Log in </Link></h4>
+        <h4> <Link to="/register" > Sing up </Link></h4>
         {this.props.children}
       </div>
     );
   }
 }
-
