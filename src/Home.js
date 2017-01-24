aee92efb7561fff072ef9faf3020c0109034949c
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-24 11:12:24 +0300

    Home page problem resolved

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,13 @@ export default class Home extends Component {
     constructor() {
       super();
     
-   
+    
     }   
-      const greetings =  'WELCOME TO ALEKS\'S BLOG'; 
+
   render() {
     return (
       <div className = "container" >
-        <h1> {greetings} </h1>
-        {user && <h3> You are currntly logged in as {this.props.user.username} </h3>}
+        <h1> WELCOME TO ALEKS\'S BLOG </h1>
         <h4> <Link to = "/login" > Log in </Link></h4>
         <h4> <Link to="/register" > Sing up </Link></h4>
         {this.props.children}
