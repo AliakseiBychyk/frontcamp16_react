6797d3028e1d13506a70feddb3fac6a3427a50ee
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-24 13:08:34 +0300

    changes in home page

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+const greetings = 'WELCOME TO ALEKS\'S BLOG';
+
 export default class Home extends Component {
     constructor() {
       super();
@@ -9,11 +11,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className = "container" >
-<<<<<<< HEAD
-        <h1> "WELCOME TO ALEKS'S BLOG" </h1>
-=======
-        <h1> "WELCOME TO ALEKS\'S BLOG" </h1>
->>>>>>> c6e409df9959723dd5a067d2dce09ee481d5803a
+        <h1> {greetings} </h1>
         <h4> <Link to = "/login" > Log in </Link></h4>
         <h4> <Link to="/register" > Sing up </Link></h4>
         {this.props.children}
