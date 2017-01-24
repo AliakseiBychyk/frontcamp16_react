4482c19c399459a4e95212547bbed58de5eab248
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-24 11:09:44 +0300

    Home page changes

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router';
 
 export default class Home extends Component {
     constructor() {
-        super();
-    }
-
-    const greetings = "WELCOME TO ALEKS\'S BLOG";
-
+      super();
+    
+   
+    }   
+      const greetings =  'WELCOME TO ALEKS\'S BLOG'; 
   render() {
     return (
       <div className = "container" >
         <h1> {greetings} </h1>
-        {user && < h3 > You are currntly logged in as {user.username} < /h3>}
+        {user && <h3> You are currntly logged in as {this.props.user.username} </h3>}
         <h4> <Link to = "/login" > Log in </Link></h4>
         <h4> <Link to="/register" > Sing up </Link></h4>
         {this.props.children}
